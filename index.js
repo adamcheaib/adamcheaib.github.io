3fadfa590f4e4655553ba44d74bc21c41551f504
Adam Cheaib <adam-cheaib@hotmail.com>
2023-08-24 01:52:42 -0400

    github deployment

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
 "use strict"
 
-async function navigationButtons(event) {
-    const eventLink = event.target.getAttribute("page-id");
+console.log(document.querySelectorAll("*"));
 
-    if (window.location.href.includes("displaycar")) {
-        window.location.href = "../?pageid=" + eventLink;
-    } else {
-        window.location.href = "./?pageid=" + eventLink;
-    }
-}
 
-const navButtons = document.querySelectorAll("#mainNav > menu > div");
-navButtons.forEach(navButton => navButton.addEventListener("click", navigationButtons));
+window.addEventListener("load", () => {
+    document.body.innerHTML += "<h1>Loaded!</h1>";
+    const div = document.createElement("div");
+    let int = 0;
 
-const hamburgerMenu = document.getElementById("hamburgerMenu");
-const dialog = document.querySelector("dialog");
-console.log(dialog);
-
-hamburgerMenu.addEventListener("click", showPhoneMenu)
-
-function showPhoneMenu(event) {
-    dialog.innerHTML = "";
-    dialog.className = "";
-    dialog.id = "";
-    dialog.id = "phoneMenuPopUp";
-
-    dialog.innerHTML = `
-    <div>
-        <span>X</span>
-        <ul>
-            <li page-id="1">Home</li>
-            <li page-id="2">Inventory</li>
-            <li page-id="3">Contact</li>
-        </ul>
-    </div>
-    `;
-
-    dialog.querySelectorAll("ul > li").forEach(li => li.addEventListener("click", navigationButtons));
-    dialog.querySelector("span").addEventListener("click", () => { dialog.className = ""; dialog.id = "";  dialog.close()});
-
-    dialog.showModal();
-}
+    setInterval(() => {
+        div.textContent = int;
+        int++;
+    }, 1000);
+})
